9b89321f40d59400603fbf787861abd6dbfa879b
R <vonshednob-5639@kakaomilchkuh.de>
2018-11-22 15:14:18 +0100

    fixing error #1

--- prog.py
+++ prog.py
@@ -18,9 +18,7 @@ def get_word_lengths(s):
     Returns a list of integers representing
     the word lengths in string s.
     """
-    # uncomment next line in step 9
-#   return [len(word) for word in s.split()]
-    return None
+    return [len(word) for word in s.split()]
 
 
 # uncomment this function in step 6
